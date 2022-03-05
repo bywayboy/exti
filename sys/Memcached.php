28d74192ed86e717b6ee0a48ff3c986b98798541
bywayboy <bywayboy@qq.com>
2022-03-05 14:49:47 +0800

    序列化方法 兼容PHP原生Memcached

--- sys/Memcached.php
+++ sys/Memcached.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace sys;
 
+use Closure;
 use Swoole\ConnectionPool;
 use sys\exception\MemcachedException;
 
@@ -10,20 +11,63 @@ class Memcached {
 
     protected string $key;
     protected $pool;
+
+    protected Closure $serialize;
+    protected Closure $unserialize;
+
     # 连接池
     protected static array $conn_pool = [];
 
-    /**
+        /**
      * 创建|打开一个Memcached连接池.
      * @param string $host 主机IP地址
      * @param int $port 主机端口.
      * @param int $size 连接池的连接个数 第一次创建时有效.
+     * @param ?Closure $serialize
+     * @param ?Closure $unserialize
      */
-    public function __construct(string $host, int $port = 11211, int $size = 16)
+    public function __construct(string $host, int $port = 11211, int $size = 16, ?Closure $serialize = null, ?Closure $unserialize = null)
     {
+        # 兼容 php_memcached.so 扩展的序列化方法
+        $this->serialize = $serialize ?? function(mixed $value, int &$flags) : string {
+            $type = gettype($value);
+            switch($type){
+            case 'string':
+                $flags = 0;
+                return $value;
+            case 'integer':
+                $flags = 1;
+                return $value;
+            case 'double':
+                $flags = 2;
+                return $value;
+            case 'boolean':
+                $flags = 3;
+                return $value?1:0;
+            default:
+                $flags = 4;
+                return serialize($value);
+            }
+        };
+
+        $this->unserialize = $unserialize ?? function(int $flag, string $value){
+            #echo "FLAG={$flag}, VALUES={$value}\n";
+            switch($flag){
+            case 0:
+                return $value;
+            case 1:
+                return intval($value);
+            case 2:
+                return floatval($value);
+            case 3:
+                return $value=='1'?true:false;
+            default:
+                return unserialize($value);
+            }
+        };
+
         $this->key = "{$host}:{$port}";
         $this->pool = static::$conn_pool[$this->key] ?? null;
-        
         if(null === $this->pool){
             $this->pool = static::$conn_pool[$this->key] = new ConnectionPool(function() use($host, $port){
                 $client = new \Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
@@ -31,17 +75,18 @@ class Memcached {
                     'open_eof_check'=>true,
                     'package_eof'=>"\r\n"
                 ));
-                echo "connect memcached: " . $client->connect($host, $port) . "\n";
+                #echo "connect memcached: " . $client->connect($host, $port) . "\n";
                 return $client;
             }, $size);
         }
     }
 
     protected function store(string $cmd, string $key, mixed $value, int $expire = 0) : bool {
-        $value = serialize($value);
+        $flags = 0;
+        $value = ($this->serialize)($value, $flags);
         $len = strlen($value);
 
-        $cmd = "{$cmd} {$key} 0 {$expire} {$len}\r\n{$value}\r\n";
+        $cmd = "{$cmd} {$key} {$flags} {$expire} {$len}\r\n{$value}\r\n";
 
         $conn = $this->pool->get();
         if(strlen($cmd) === $conn->send($cmd)){
@@ -76,9 +121,9 @@ class Memcached {
 
                         $payload[] = $r;
                         $recived += strlen($r);
-                        if($recived >= $bytes){
+                        if($recived >= $bytes + 5){ # 数据以 END\r\n 结束 因此需增加5个 byte
                             $this->pool->put($conn);
-                            return unserialize(substr(implode('', $payload), 0, intval($bytes)));
+                            return ($this->unserialize)(intval($flags), substr(implode('', $payload), 0, intval($bytes)));
                         }
                     }
                 }else{
@@ -111,7 +156,7 @@ class Memcached {
      * @param string $key 要删除的数据的键值
      */
     public function del(string $key) : bool {
-        $cmd = "del {$key}\r\n";
+        $cmd = "delete {$key}\r\n";
         $conn = $this->pool->get();
         if(strlen($cmd) === $conn->send($cmd)){
             if(false !== $ret = $conn->recv()){
