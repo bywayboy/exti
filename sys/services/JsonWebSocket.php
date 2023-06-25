2b5b3f4a12aa27ca17b8c779bc01287bee136861
bywayboy <bywayboy@qq.com>
2023-06-25 10:30:58 +0800

    websocket 可以远程调用客户端.

--- sys/services/JsonWebSocket.php
+++ sys/services/JsonWebSocket.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace sys\services;
 
 use Swoole\Http\Request;
+use Swoole\Timer;
+use sys\Log;
 use Throwable;
 
 class JsonWebSocket extends WebSocket
@@ -11,6 +13,8 @@ class JsonWebSocket extends WebSocket
     protected mixed $service;
     protected Request $request;
     
+    private int $_SEQ = 1;
+    private array $callbacks = [];
     public function __construct(Request $request, mixed $service)
     {
         $this->request = $request;
@@ -38,7 +42,7 @@ class JsonWebSocket extends WebSocket
                 };
 
                 $result = call_user_func_array([$this->service, 'on'.
-                str_replace(['-','_'],['',''], ucwords($data['action'], "-_\t\r\n\f\v"))
+                str_replace(['-','_','.'],['','',''], ucwords($data['action'], "-_.\t\r\n\f\v"))
                 ], [
                     $data,
                     function(array $data) use($sender){$sender($data, false);}, 
@@ -49,19 +53,52 @@ class JsonWebSocket extends WebSocket
                 if(false === $_DONE)
                 {
                     if(is_array($result)){
-                        if(!isset($result['_SEQ'])) {
-                            $result['_SEQ'] = $_SEQ;
-                            $result['done'] = true;
-                        }
-                        $this->channel->push($result);
+                        $this->channel->push($result+['_SEQ'=>$_SEQ,'done'=>true]);
                     }elseif(is_string($result)){
                         $this->channel->push($result);
                     }
                 }
             }catch(\Throwable $e){
-                echo "EROR:" . $e->getMessage() . "\ncode:" . $e->getCode() . "\n File: ". $e->getFile() . " at line:".$e->getLine()."\nTrace: ". $e->getTraceAsString() . "\n";
+                Log::write($e->getMessage() . "\ncode:" . $e->getCode() . "\n File: ". $e->getFile() . " at line:".$e->getLine()."\nTrace: ". $e->getTraceAsString(),'ERROR');
+            }
+        }elseif(isset($data['event'])){
+            // 收到客户端的事件通知
+        }elseif(isset($data['_SEQ'])){
+            // 收到客户端的响应
+            Log::console("收到响应: ".json_encode($data, JSON_UNESCAPED_UNICODE), 'DEBUG');
+            if(isset($this->callbacks[$data['_SEQ']])) {
+                // 获取内容并清理
+                $callback = $this->callbacks[$data['_SEQ']];
+                unset($this->callbacks[$data['_SEQ']]);
+
+                // 执行响应回调
+                Timer::clear($callback[0]);
+                $callback[1]($data);
             }
         }
+    }
+
+    /**
+     * 服务器向客户端发起一个远程调用
+     */
+    public function call(string $action, null|array $data, int $timeout, \Closure $callback) {
+        $Seq = $this->_SEQ ++;
+
+        Log::console("远程调用:{$Seq}, {$action}", 'DEBUG');
+        $this->callbacks[$Seq] = [Timer::after($timeout, function() use($callback, $Seq){
+            if(isset($callbacks[$Seq])){
+                unset($callbacks[$Seq]);
+                Log::console("等待超时: {$Seq}", 'ERROR');
+                $callback(['success'=>false, 'message'=>'等待超时']);
+            }
+        }), $callback];
+
+        $this->channel->push([
+            'action'=>$action,
+            '_SEQ'=>$Seq,
+            'data'=>$data,
+            'callback'=>$callback
+        ]);
     }
 
     protected function OnBinaryMessage_(string $bin) : void {
@@ -73,7 +110,7 @@ class JsonWebSocket extends WebSocket
             return call_user_func_array([$this->service, 'afterConnected'], [$this->request, $this]);
         }catch(Throwable $e){
             # 忽略错误.
-            echo "EROR:" . $e->getMessage() . "\ncode:" . $e->getCode() . "\n File: ". $e->getFile() . " at line:".$e->getLine()."\nTrace: ". $e->getTraceAsString() . "\n";
+            Log::write($e->getMessage() . "\ncode:" . $e->getCode() . "\n File: ". $e->getFile() . " at line:".$e->getLine()."\nTrace: ". $e->getTraceAsString(),'ERROR');
         }
         return false;
     }
@@ -89,7 +126,7 @@ class JsonWebSocket extends WebSocket
             call_user_func_array([$this->service, 'afterClose'], [$this->request, $this]);
         }catch(Throwable $e){
             # 忽略错误.
-            echo "EROR:" . $e->getMessage() . "\ncode:" . $e->getCode() . "\n File: ". $e->getFile() . " at line:".$e->getLine()."\nTrace: ". $e->getTraceAsString() . "\n";
+            Log::write($e->getMessage() . "\ncode:" . $e->getCode() . "\n File: ". $e->getFile() . " at line:".$e->getLine()."\nTrace: ". $e->getTraceAsString(),'ERROR');
         }
     }
 }
