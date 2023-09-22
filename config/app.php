da02aa433b492e67a64e657dc34edbc5f0aab34a
bywayboy <bywayboy@qq.com>
2023-09-22 16:50:39 +0800

    补充默认配置项

--- config/app.php
+++ config/app.php
@@ -8,9 +8,10 @@ return [
             'enabled'       => true,                                        # 模块是否开启
             'worker_num'    =>intval(getenv('CPUS') ?? 1),                  # 工作进程数目
             'server_id'     =>intval(getenv('SERVER_ID') ?? 0),             # 服务器节点ID(每台服务器一个独立ID)
+            'worker_id'     =>intval(getenv('WORKER_ID') ?? 0),             # 计算节点ID(工作进程ID起始编号)
             'bind_address'  =>trim(getenv('BIND_ADDRESS') ?? '127.0.0.1'),  # 服务器监听IP地址
             'listen_port'   =>intval(getenv('LISTEN_PORT') ?? 8400),        # 服务器监听起始端口
-            'share_port'    => false,                                        # 每个工作进程共享起始端口, 如果设置为 false 则每个工作进程绑定独立端口 listen_port + 进程ID(从零开始) 
+            'share_port'    => false,                                       # 每个工作进程共享起始端口, 如果设置为 false 则每个工作进程绑定独立端口 listen_port + 进程ID(从零开始) 
             'user'          =>'php:www',                                    # 工作进程绑定到指定用户和组,
             'protocol'      => \sys\servers\HttpServer::class,              # 服务器角色, 这是HTTP 服务器角色
             'ssl'           =>false,                                        # 是否启用SSL安全连接
